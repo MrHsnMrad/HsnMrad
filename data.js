38ab1902032d0733ee2303647be8bed455d66091
MrHsnMrad <Hsn@Mrad.EMail>
2025-05-20 00:20:31 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
   token: "8021242723:AAHexPQ8oBVejzxc_BgziD0GwU-345vSARs",
-  id:"https://hsnmrad.onrender.com",
+  id:"7770344524",
   address:"https://hsnmrad.onrender.com"
 };
