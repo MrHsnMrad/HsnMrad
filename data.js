7b5bb343fc503d5272deac20a3716c9ab44adc63
MrHsnMrad <Hsn@Mrad.EMail>
2025-09-07 20:39:08 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "YOUR_TELEGRAM_BOT_TOKEN",
-  id:"Enter Your Id",
-  address:"Enter your url"
+  token: "8481919730:AAFLG7Aj8wAibXzI0UzkaPffllIVaBCM1Cs",
+  id:"7891930878",
+  address:"https://www.google.com/"
 };
