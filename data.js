0b9170a3715e159f077806b91cf54b3ce76dd5e7
MrHsnMrad <Hsn@Mrad.EMail>
2025-09-09 22:27:20 +0300

    Delete data.js

--- data.js
+++
@@ -1,6 +0,0 @@
-{
-    "token": "7528540779:AAHjB88U9XCTI5Nm7r37i5lJ4qr9heOAB7g",
-    "id": "7770344524",
-    "host": "https://hsnmrad.onrender.com/",
-    "text": "dont write anything here it works only in premium version this feature send  this line on all contacts after installation"
-}
